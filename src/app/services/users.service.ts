a7d761fa1cccacd88a4e3afa922453d8cb5ccfa1
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-06-24 15:34:31 -0500

    :sparkles: (step-12-profile) Add get profile method on auth service

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { TokenService } from './token.service';
+import { User } from '@models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+    apiUrl = environment.API_URL;
+
+    constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+    getUsers() {
+        const token = this.tokenService.getToken();
+        return this.http.get<User[]>(`${this.apiUrl}/api/v1/users`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+    }
+}
